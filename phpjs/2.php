c58fa27eb726cb1503cf9bf994e976c9bbd2e9b2
2DaeHee <40678414+leedh909@users.noreply.github.com>
2020-01-14 16:12:00 +0900

    2020.1.14
    php와 javascript 기초내용

---
+++ phpjs/2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>javaScript</h1>
+    <script>
+       document.write("10"+"10");
+    </script>
+    <h2>PHP</h2>
+    <?php echo "10"."10"; ?>
+  </body>
+</html>
---
+++ phpjs/5.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>javaScript</h1>
+    <script type="text/javascript">
+      document.write(2==1);
+
+    </script>
+    <h1>PHP</h1>
+    <?php
+      echo 1==1;
+    ?>
+  </body>
+</html>
