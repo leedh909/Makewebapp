284d160f96cae42848a4dce6ee6bdc1dd5de6ab3
2DaeHee <40678414+leedh909@users.noreply.github.com>
2020-01-16 16:51:07 +0900

    2020.1.16

---
+++ phpjs/12.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+<h1>javascript</h1>
+<script>
+  function a(){
+    document.write("hello javascript function");
+
+  }
+  a();
+</script>
+
+<h1>php</h1>
+<?php
+ function a(){
+   echo "hello php function";
+ }
+ a();
+?>
+
+  </body>
+</html>
