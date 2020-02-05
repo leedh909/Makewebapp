d4f718b4eae8632beefe50c1192aa962dc7be2be
2DaeHee <40678414+leedh909@users.noreply.github.com>
2020-02-05 18:28:07 +0900

    2020.2.5

--- php/index.php
+++ php/index.php
@@ -20,7 +20,7 @@ $result = mysqli_query($conn,"SELECT * FROM topic");
 	<ol>
 		<?php
 		while($row = mysqli_fetch_assoc($result)){
-			echo '<li><a href="http://localhost/php/index.php?id='.$row['id'].'">'.$row['title'].'</a></li>'."\n";
+			echo '<li><a href="http://localhost/php/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
 		}
 		 ?>
 	</ol>
@@ -34,10 +34,12 @@ $result = mysqli_query($conn,"SELECT * FROM topic");
 	<?php
   if(empty($_GET['id'])===false){
   $sql = 'SELECT * FROM topic WHERE id='.$_GET['id'];
+  $sql = "SELECT topic.id, title,name,description FROM topic LEFT JOIN user ON topic.author = user.id WHERE topic.id=".$_GET['id'];
   $result = mysqli_query($conn, $sql);
   $row = mysqli_fetch_assoc($result);
-  echo '<h2>'.$row['title'].'</h2>';
-  echo $row['description'];
+  echo '<h2>'.htmlspecialchars($row['title']).'</h2>';
+  echo '<p>'.htmlspecialchars($row['name']).'</p>';
+  echo strip_tags($row['description'],'<a><h1><h2><h3><h4><h5><ul><ol><li>');
   }
 	 ?>
 </article>
